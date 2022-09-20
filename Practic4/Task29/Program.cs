5c238ea0ad3a92db0d5bd9e21bcaa5a364b28e3e
Greenader <you@example.com>
2022-09-20 21:06:29 +0700

    Practic3

---
+++ Practic4/Task29/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.WriteLine("Введите 8 элементов через 'Enter': ");
+int[] nums = new int[8];
+for (int i = 0; i < 8; i++)
+{
+    nums[i] = int.Parse(Console.ReadLine());
+}
+
+Console.Write("\nВведенный массив : [ ");
+
+for (int i = 0; i < 8; i++)
+{
+    Console.Write(nums[i] + " ");
+}
+Console.WriteLine("]");
